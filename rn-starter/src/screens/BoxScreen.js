98b92ed2dcd392dc78962b980c597e92555f5cc1
Anand Lilhare <anandlilhare69@gmail.com>
2022-02-10 18:22:40 +0530

    working with flex box height and absoluteposition

--- rn-starter/src/screens/BoxScreen.js
+++ rn-starter/src/screens/BoxScreen.js
@@ -3,7 +3,9 @@ import {Text, View, StyleSheet } from 'react-native';
 
 const BoxScreen = () => {
     return ( <View style={styles.viewStyle}>
-        <Text style={styles.textStyle}>This is Box Screen Demo</Text>
+        <Text style={styles.textOneStyle}>Chiild 1</Text>
+        <Text style={styles.textTwoStyle}>Child 2</Text>
+        <Text style={styles.textThreeStyle}>Child 3</Text>
     </View>
     )
 };
@@ -11,13 +13,27 @@ const BoxScreen = () => {
 const styles = StyleSheet.create({
     viewStyle : {
        borderWidth: 2,
-       borderColor: 'black'
+       borderColor: 'black',
+       height:200,
+       
+        
     },
-    textStyle: {
-      borderWidth:2,
+    textOneStyle: {
+      borderWidth:3,
       borderColor:'red',
-      margin:30
-    }
+      
+    },
+    textTwoStyle: {
+        borderWidth:3,
+        borderColor:'red',
+        fontSize:18,
+        ...StyleSheet.absoluteFillObject
+       },
+      textThreeStyle: {
+        borderWidth:3,
+        borderColor:'red',
+      
+      }
 });
 
 export default BoxScreen;
